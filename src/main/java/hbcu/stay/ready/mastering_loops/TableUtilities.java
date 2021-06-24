769bc8229c66e4e8c1cf90ab9d33b4304ec2760c
Emeka Okafor <eokafor78@Emekas-MBP-2.fios-router.home>
2021-06-24 11:39:05 -0400

    All tests done for TableUtilitiesTest

--- src/main/java/hbcu/stay/ready/mastering_loops/TableUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TableUtilities.java
@@ -2,14 +2,70 @@ package hbcu.stay.ready.mastering_loops;
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        String expected = "";
+
+        for (int i = 1; i <= 5; i++) {
+            expected += getSmallMultiplicationTableRoW(i) + "\n";
+        }
+
+        return expected;
+    }
+
+
+    private static String getSmallMultiplicationTableRoW(int row){
+        String expected = "";
+
+        for (int j = 1; j <= 5; j++) {
+            expected += String.format("%3s |", row * j);
+        }
+        return expected;
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+
+        String expected = "";
+
+
+        for (int i = 1; i <= 10; i++) {
+            expected += getLargeMultiplicationTableRow(i) + "\n";
+        }
+
+
+        return expected;
     }
 
-    public static String getMultiplicationTable(int tableSize) {
-        return null;
+    private static String getLargeMultiplicationTableRow ( int row) {
+
+        String expected = "";
+
+        for (int j = 1; j <= 10; j++){
+            expected += String.format("%3s |", row * j);
+
+
+        }
+        return expected;
+    }
+
+    public static String getMultiplicationTable ( int tableSize){
+
+        String table = "";
+
+
+        for (int x = 1; x <= tableSize; x++) {
+            table += getMultiplicationTableRow(x, tableSize) + "\n";
+        }
+
+
+        return table;
+    }
+
+    private static String getMultiplicationTableRow ( int row, int tableSize){
+        String table = "";
+
+        for (int y = 1; y <= tableSize; y++) {
+            table += String.format("%3s |", y * row);
+        }
+
+        return table;
     }
 }
