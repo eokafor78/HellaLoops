44cc291c91d8e3d780ecfb725cae054013ed5e26
Emeka Okafor <eokafor78@Emekas-MBP-2.fios-router.home>
2021-06-24 12:33:24 -0400

    Most tests done for TriangleUtilitiesTest

--- src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/TriangleUtilities.java
@@ -3,18 +3,78 @@ package hbcu.stay.ready.mastering_loops;
 public class TriangleUtilities {
 
     public static String getTriangle(int numberOfRows) {
-        return null;
+        String triangle = "";
+
+        for (int i = 0; i < numberOfRows; i++) {
+            triangle += getTriangleHelper(i) + "\n";
+        }
+        return triangle;
+    }
+
+    private static String getTriangleHelper(int row) {
+        String triangle = "";
+
+        for (int j = -1; j <= row; j++) {
+            triangle += "*";
+        }
+        return triangle;
     }
 
     public static String getRow(int numberOfStars) {
-        return null;
+        String row = "";
+
+        for (int i = 1; i <= numberOfStars; i++) {
+            row += "*";
+        }
+        return row;
     }
 
     public static String getSmallTriangle() {
-        return null;
+
+        String triangle = "";
+
+        for (int i = 1; i <= 4; i++) {
+            triangle += getSmallTriangleHelper(i) + "\n";
+
+
+        }
+
+        return triangle;
+    }
+
+    private static String getSmallTriangleHelper(int row) {
+        String triangle = "";
+
+        for (int j = 0; j < row; j++) {
+            triangle += "*";
+
+        }
+
+        return triangle;
+
     }
 
     public static String getLargeTriangle() {
-        return null;
+
+        String triangle = "";
+
+        for (int i = 1; i <= 9; i++) {
+            triangle += getLargeTriangleHelper(i) + "\n";
+
+        }
+
+        return triangle;
+    }
+
+
+    private static String getLargeTriangleHelper(int row) {
+        String triangle = "";
+
+        for (int j = 0; j < row; j++) {
+            triangle += "*";
+
+        }
+
+        return triangle;
     }
 }
