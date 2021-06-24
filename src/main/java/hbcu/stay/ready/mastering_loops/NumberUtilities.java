111bfa0f13182264228f69073126557d589e2fd5
Emeka Okafor <eokafor78@Emekas-MBP-2.fios-router.home>
2021-06-24 11:16:09 -0400

    Most tests done for NumberUtilitiesTest

--- src/main/java/hbcu/stay/ready/mastering_loops/NumberUtilities.java
+++ src/main/java/hbcu/stay/ready/mastering_loops/NumberUtilities.java
@@ -3,34 +3,67 @@ package hbcu.stay.ready.mastering_loops;
 
 public class NumberUtilities {
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String number = "";
+        for(int i = start; i < stop; i++) {
+            if(i % 2 == 0) {
+                number += i;
+            }
+        }
+        return number;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String number = "";
+        for(int i = start; i < stop; i++) {
+            if(i % 2 != 0) {
+                number += i;
+            }
+        }
+        return number;
     }
 
 
     public static String getSquareNumbers(int start, int stop, int step) {
-        return null;
+        String number = "";
+//        for(int i = start; i < stop; step *= step) {
+//             number += step;
+//        }
+        return number;
     }
 
-    public static String getRange(int start) {
-        return null;
+    public static String getRange(int stop) {
+        String number = "";
+        for(int i = 0; i < stop; i++) {
+            number += i;
+        }
+        return number;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String number = "";
+        for(int i = start; i < stop; i++) {
+            number += i;
+        }
+        return number;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String number = "";
+        for(int i = start; i < stop; i += step) {
+            number += i;
+        }
+        return number;
     }
 
 
     public static String getExponentiations(int start, int stop, int step, int exponent) {
-        return null;
+        String result = "";
+//        for(int i = start; i < stop; step += start) {
+//            double total = Math.pow(i, exponent);
+//            result += (double)total;
+//        }
+        return result;
     }
 }
